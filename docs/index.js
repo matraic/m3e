dd89e25ea9f7ad9b022cf9ad1fe4e8b139a24890
matraic <matraic@yahoo.com>
2025-11-30 00:48:23 -0500

    docs: added color, scheme and directionality settings

--- docs/index.js
+++ docs/index.js
@@ -21,4 +21,24 @@ window.addEventListener("DOMContentLoaded", () => {
   const allowedPathRegex = /^([a-zA-Z0-9_-]+\/)*[a-zA-Z0-9_-]+\.html$/;
   const requestedPath = location.hash ? location.hash.substring(2) : "";
   frame.src = allowedPathRegex.test(requestedPath) ? requestedPath : "getting-started/overview.html";
+
+  const color = document.querySelector("#color");
+  color.value = document.querySelector("m3e-theme").color;
+  color.addEventListener("change", () => {
+    document.querySelector("m3e-theme").color = color.value;
+    frame.contentWindow.postMessage({ type: "color-change", color: color.value }, "*");
+  });
+
+  const colorSchemeButton = document.querySelector("#color-scheme-button");
+  colorSchemeButton?.addEventListener("change", () => {
+    document.querySelector("m3e-theme").scheme = colorSchemeButton.value;
+
+    const frame = document.querySelector("#content-frame");
+    frame.contentWindow.postMessage({ type: "color-scheme-change", scheme: colorSchemeButton.value }, "*");
+  });
+
+  const directionalityButton = document.querySelector("#directionality-button");
+  directionalityButton?.addEventListener("change", () => {
+    document.documentElement.dir = directionalityButton.value;
+  });
 });
--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
   if (window.parent && window.parent.document && window.parent.document.documentElement) {
     document.documentElement.dir = window.parent.document.documentElement.dir;
+
+    const theme = document.querySelector("m3e-theme");
+    const parentTheme = window.parent.document.querySelector("m3e-theme");
+    theme.scheme = parentTheme.scheme;
+    theme.color = parentTheme.color;
   }
 });
 
 window.addEventListener("message", (e) => {
-  if (e.data?.type === "direction-change") {
-    document.documentElement.dir = e.data.dir;
+  switch (e.data?.type) {
+    case "color-change":
+      document.querySelector("m3e-theme").color = e.data.color;
+      break;
+
+    case "color-scheme-change":
+      document.querySelector("m3e-theme").scheme = e.data.scheme;
+      break;
+
+    case "direction-change":
+      document.documentElement.dir = e.data.dir;
+      break;
   }
 });
