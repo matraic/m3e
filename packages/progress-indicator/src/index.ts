66577eb5d73f18a3366f80df800f921a50f7ceb6
matraic <matraic@yahoo.com>
2026-02-21 01:57:11 -0500

    fix(@m3e/progress-indicator): corrected missing ProgressIndicatorVariant export

--- packages/progress-indicator/src/index.ts
+++ packages/progress-indicator/src/index.ts
@@ -2,3 +2,4 @@ export * from "./CircularProgressIndicatorElement";
 export * from "./LinearProgressIndicatorElement";
 export * from "./LinearProgressMode";
 export * from "./ProgressElementIndicatorBase";
+export * from "./ProgressIndicatorVariant";
