d94bb52765601b791b1ce77813e3659f463c05fb
matraic <matraic@yahoo.com>
2026-05-02 02:28:37 -0400

    fix(breadcrumb): correct item jsdoc for click

--- packages/web/src/breadcrumb/BreadcrumbItemElement.ts
+++ packages/web/src/breadcrumb/BreadcrumbItemElement.ts
@@ -30,6 +30,8 @@ import "./BreadcrumbItemButtonElement";
  * @attr download - A value indicating whether the internal link target will be downloaded, optionally specifying a file name.
  * @attr rel - The relationship between the internal link target and the document.
  *
+ * @fires click - Emitted when the element is clicked.
+ *
  * @cssprop --m3e-breadcrumb-item-shape - Shape of the internal breadcrumb item button.
  * @cssprop --m3e-breadcrumb-item-container-height - Height of the internal breadcrumb item button container.
  * @cssprop --m3e-breadcrumb-item-icon-color - Color of breadcrumb item icon-only content.
