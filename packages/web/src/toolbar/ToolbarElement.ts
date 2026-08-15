matraic/m3e

commit 2d9c9b8f6b895c8ca436ace21ead244769fc2b7c

fix(app-bar, calendar, list, paginator, search, toolbar): apply 48px action-slot layout to icon buttons (closes #106)

packages/web/src/toolbar/ToolbarElement.ts
@@ -80,6 +80,8 @@ export class M3eToolbarElement extends Vertical(Role(LitElement, "toolbar")) {
       align-items: center;
       box-sizing: border-box;
       border-radius: inherit;
+      --_icon-button-inline-size: auto;
+      --_icon-button-min-size: 48px;
     }
     :host(:not([vertical])) {
       height: fit-content;

packages/web/src/app-bar/styles/AppBarStyle.ts
@@ -42,6 +42,8 @@ export const AppBarStyle: CSSResult = css`
     display: flex;
     flex: none;
     align-items: center;
+    --_icon-button-inline-size: auto;
+    --_icon-button-min-size: 48px;
   }
   .leading-icon {
     min-width: var(--_leading-icon-min-width);

packages/web/src/calendar/CalendarElement.ts
@@ -130,6 +130,8 @@ export class M3eCalendarElement extends LitElement {
       height: 56px;
       display: flex;
       align-items: center;
+      --_icon-button-inline-size: auto;
+      --_icon-button-min-size: 48px;
       --m3e-text-button-label-text-color: var(
         --m3e-calendar-period-button-text-color,
         ${DesignToken.color.onSurfaceVariant}

packages/web/src/icon-button/IconButtonElement.ts
@@ -431,23 +431,25 @@ export class M3eIconButtonElement extends KeyboardClick(
 
   /** @inheritdoc */
   override render(): unknown {
-    return html`<div class="base">
-      <m3e-state-layer class="state-layer" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-state-layer>
-      <m3e-elevation class="elevation" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-elevation>
-      <m3e-focus-ring class="focus-ring" ?disabled="${this.disabled}"></m3e-focus-ring>
-      <m3e-ripple class="ripple" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-ripple>
-      <div class="touch" aria-hidden="true"></div>
-      ${this[renderPseudoLink]()}
-      <div class="wrapper">
-        ${this.toggle
-          ? html`<slot
-              class="icon"
-              name="selected"
-              aria-hidden="true"
-              @slotchange=${this.#handleSelectedIconSlotChange}
-            ></slot>`
-          : nothing}
-        <slot class="icon" aria-hidden="true"></slot>
+    return html`<div class="layout">
+      <div class="base">
+        <m3e-state-layer class="state-layer" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-state-layer>
+        <m3e-elevation class="elevation" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-elevation>
+        <m3e-focus-ring class="focus-ring" ?disabled="${this.disabled}"></m3e-focus-ring>
+        <m3e-ripple class="ripple" ?disabled="${this.disabled || this.disabledInteractive}"></m3e-ripple>
+        <div class="touch" aria-hidden="true"></div>
+        ${this[renderPseudoLink]()}
+        <div class="wrapper">
+          ${this.toggle
+            ? html`<slot
+                class="icon"
+                name="selected"
+                aria-hidden="true"
+                @slotchange=${this.#handleSelectedIconSlotChange}
+              ></slot>`
+            : nothing}
+          <slot class="icon" aria-hidden="true"></slot>
+        </div>
       </div>
     </div>`;
   }

packages/web/src/icon-button/styles/IconButtonStyle.ts
@@ -16,14 +16,22 @@ export const IconButtonStyle: CSSResultGroup = css`
   :host([hidden]) {
     display: none;
   }
+  .layout,
   .base {
     box-sizing: border-box;
     vertical-align: middle;
     display: inline-flex;
     align-items: center;
     justify-content: center;
+  }
+  .layout {
+    width: var(--_icon-button-inline-size, 100%);
+    min-width: var(--_icon-button-min-size);
+    min-height: var(--_icon-button-min-size);
+  }
+  .base {
     position: relative;
-    width: 100%;
+    width: var(--_icon-button-inline-size, 100%);
     transition: ${unsafeCSS(
       `background-color ${DesignToken.motion.duration.short4} ${DesignToken.motion.easing.standard}`,
     )};

packages/web/src/list/ListItemElement.ts
@@ -249,6 +249,11 @@ export class M3eListItemElement extends ReconnectedCallback(AttachInternals(Role
       min-width: var(--_list-item-trailing-reserved-space, 0px);
       margin-inline-end: calc(0px - var(--_list-item-trailing-reserved-outset, 0px));
     }
+    slot[name="leading"],
+    slot[name="leading"] {
+      --_icon-button-inline-size: auto;
+      --_icon-button-min-size: 48px;
+    }
     ::slotted(span[slot="trailing"]),
     ::slotted([slot="trailing-supporting-text"]) {
       white-space: nowrap;

packages/web/src/paginator/PaginatorElement.ts
@@ -125,6 +125,8 @@ export class M3ePaginatorElement extends SupportsDirectionality(AttachInternals(
     .range-actions {
       display: flex;
       align-items: center;
+      --_icon-button-inline-size: auto;
+      --_icon-button-min-size: 48px;
     }
     ::slotted([slot="first-page-icon"]),
     ::slotted([slot="previous-page-icon"]),

packages/web/src/search/styles/SearchBarStyle.ts
@@ -41,6 +41,8 @@ export const SearchBarStyle: CSSResultGroup = css`
     height: 100%;
     border-radius: ${SearchBarToken.containerShape};
     background-color: ${SearchBarToken.containerColor};
+    --_icon-button-inline-size: auto;
+    --_icon-button-min-size: 48px;
   }
   .clear {
     display: flex;

commit 1a2a942c5e1e67b56ff3ae49e3680d179daa8218

fix(toolbar): correct density support (regression from 2d9c9b8, see #106)

packages/web/src/toolbar/ToolbarElement.ts
@@ -116,23 +116,32 @@ export class M3eToolbarElement extends Vertical(Role(AttachInternals(LitElement)
         --m3e-toolbar-rounded-trailing-space,
         var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
       );
-      padding-block-start: var(
-        --m3e-toolbar-rounded-top-space,
-        var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+      padding-block-start: calc(
+        var(--m3e-toolbar-rounded-top-space, var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})) +
+          ${DesignToken.density.calc(-3)}
       );
-      padding-block-end: var(
-        --m3e-toolbar-rounded-bottom-space,
-        var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+      padding-block-end: calc(
+        var(
+            --m3e-toolbar-rounded-bottom-space,
+            var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+          ) +
+          ${DesignToken.density.calc(-3)}
       );
     }
     :host([vertical]:is(:state(--rounded), :--rounded)) .base {
-      padding-block-start: var(
-        --m3e-toolbar-rounded-leading-space,
-        var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+      padding-block-start: calc(
+        var(
+            --m3e-toolbar-rounded-leading-space,
+            var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+          ) +
+          ${DesignToken.density.calc(-3)}
       );
-      padding-block-end: var(
-        --m3e-toolbar-rounded-trailing-space,
-        var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+      padding-block-end: calc(
+        var(
+            --m3e-toolbar-rounded-trailing-space,
+            var(--m3e-toolbar-rounded-padding, ${DesignToken.measurement.space100})
+          ) +
+          ${DesignToken.density.calc(-3)}
       );
       padding-inline-start: var(
         --m3e-toolbar-rounded-top-space,
@@ -152,17 +161,27 @@ export class M3eToolbarElement extends Vertical(Role(AttachInternals(LitElement)
         --m3e-toolbar-square-trailing-space,
         var(--m3e-toolbar-square-padding, ${DesignToken.measurement.space200})
       );
-      padding-block-start: var(--m3e-toolbar-square-top-space, ${DesignToken.measurement.space100});
-      padding-block-end: var(--m3e-toolbar-square-bottom-space, ${DesignToken.measurement.space100});
+      padding-block-start: calc(
+        var(--m3e-toolbar-square-top-space, ${DesignToken.measurement.space100}) + ${DesignToken.density.calc(-3)}
+      );
+      padding-block-end: calc(
+        var(--m3e-toolbar-square-bottom-space, ${DesignToken.measurement.space100}) + ${DesignToken.density.calc(-3)}
+      );
     }
     :host([vertical]:is(:state(--square), :--square)) .base {
-      padding-block-start: var(
-        --m3e-toolbar-square-leading-space,
-        var(--m3e-toolbar-square-padding, ${DesignToken.measurement.space200})
+      padding-block-start: calc(
+        var(
+            --m3e-toolbar-square-leading-space,
+            var(--m3e-toolbar-square-padding, ${DesignToken.measurement.space200})
+          ) +
+          ${DesignToken.density.calc(-3)}
       );
-      padding-block-end: var(
-        --m3e-toolbar-square-trailing-space,
-        var(--m3e-toolbar-square-padding, ${DesignToken.measurement.space200})
+      padding-block-end: calc(
+        var(
+            --m3e-toolbar-square-trailing-space,
+            var(--m3e-toolbar-square-padding, ${DesignToken.measurement.space200})
+          ) +
+          ${DesignToken.density.calc(-3)}
       );
       padding-inline-start: var(--m3e-toolbar-square-top-space, ${DesignToken.measurement.space100});
       padding-inline-end: var(--m3e-toolbar-square-bottom-space, ${DesignToken.measurement.space100});