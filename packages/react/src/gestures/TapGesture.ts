672a7a45129239125a590b0ea002b8a1389b5a45
matraic <matraic@yahoo.com>
2026-09-13 22:27:36 -0400

    refactor(gestures): complete gesture engine overhaul (see #101)

--- packages/react/src/gestures/TapGesture.ts
+++ packages/react/src/gestures/TapGesture.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { createComponent, EventName } from "@lit/react";
 
 import { M3eTapGestureElement, TapGestureDetail } from "@m3e/web/gestures/tap";
-export type { TapGestureDetail } from "@m3e/web/gestures/tap";
+export type { TapGestureDetail, TapGestureOptions } from "@m3e/web/gestures/tap";
 
 /**
  * React binding for the `m3e-tap-gesture` Web Component from `@m3e/web/gestures/tap`.
